dd60578b4be0a116c4586c5f79f77ee380c9911c
Aanya <114793986+AanyaPanigrahi@users.noreply.github.com>
2022-11-28 15:25:32 +0530

    Update a.Assigning.py

--- 2. variables/a.Assigning.py
+++ 2. variables/a.Assigning.py
@@ -32,3 +32,11 @@ print(x,y,z) #gives same output i.e. printing multiple strings as my name is
 #but print (x+y+z) will not give same output
 print (x+y+z) #combines it to a single string as mynameis
 
+#concatenation of str - adding str datatypes
+x='hey '
+y='guys'
+z=x+y
+print(z) #hey guys
+#str can only be  added to str i.e we can't add int to it
+#but an int can be multiplied -
+print('hey'*3) #heyheyhey
