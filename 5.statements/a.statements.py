f6a62daf630a7b7930f40dff2093a815941dcfb3
Aanya <114793986+AanyaPanigrahi@users.noreply.github.com>
2022-12-19 09:33:10 +0530

    Update and rename statements.py to a.statements.py

--- 5.statements/a.statements.py
+++ 5.statements/a.statements.py
@@ -40,33 +40,6 @@ else:
        
 # AND / OR --> check two conditions at once
 
-#AND 
-name='ankita'
-age=20
-if name=='ankita' and age==20:
-       print('condition True')
-else:
-       print('condition False')
-#both conditions need to match for it to be true
-
-#OR
-name='adyasa'
-age=19
-if name=='adyasa'or age==18:
-       print('YASS TRUE')
-else:
-       print('NOPE FALSE')
-#atleast one condition needs to be true for the outcome to come true
-
-#exercise - shinchan movie
-name=input('your name: ')
-age=int(input('your age: '))
-if (name[0]=='a' or name[0]=='A') and age>=10:
-       print('you can watch dark tama tama')
-else:
-       print('you cannot watch dark tama tama')
-
-
 #if-elif-else STATEMENT
 age=int(input('enter your age: '))
 if age==0 or age<0:
