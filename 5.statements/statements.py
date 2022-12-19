06deebaec12b1f80b42359558b6b770103711fd4
Aanya <114793986+AanyaPanigrahi@users.noreply.github.com>
2022-12-19 09:23:30 +0530

    Update statements.py

--- 5.statements/statements.py
+++ 5.statements/statements.py
@@ -36,3 +36,46 @@ else:
               print("Too high")
        #in place of else we can put other if condition as
        #if user_input > winning_number  
+       
+       
+# AND / OR --> check two conditions at once
+
+#AND 
+name='ankita'
+age=20
+if name=='ankita' and age==20:
+       print('condition True')
+else:
+       print('condition False')
+#both conditions need to match for it to be true
+
+#OR
+name='adyasa'
+age=19
+if name=='adyasa'or age==18:
+       print('YASS TRUE')
+else:
+       print('NOPE FALSE')
+#atleast one condition needs to be true for the outcome to come true
+
+#exercise - shinchan movie
+name=input('your name: ')
+age=int(input('your age: '))
+if (name[0]=='a' or name[0]=='A') and age>=10:
+       print('you can watch dark tama tama')
+else:
+       print('you cannot watch dark tama tama')
+
+
+#if-elif-else STATEMENT
+age=int(input('enter your age: '))
+if age==0 or age<0:
+       print("you can't watch")
+elif 0<age<=3:
+       print('ticket price: free')
+elif 3<age<10:
+       print('ticket price: 150')
+elif 10<age<60:
+       print('ticket price: 250')
+else:
+       print('ticket price: 100')
