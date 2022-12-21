42c513315959519ef91030cf4808d222a65a66bd
Aanya <114793986+AanyaPanigrahi@users.noreply.github.com>
2022-12-21 23:37:30 +0530

    Add files via upload

---
+++ 6.loops.py/b.infinite.py
@@ -0,0 +1,14 @@
+#INFINITE LOOP
+
+#i=0
+#while i<=10:
+    #print('hello')
+    #i+=1 if not mentioned
+    #it will be an infinite loop
+#to stop this infinite loop press ctrl+c
+
+#to have it intentionally use boolean True as
+
+#while True:
+        #print('zzzz')
+        
