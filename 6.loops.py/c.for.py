6cf611bf181c5e8e98e09e139db259a04a87716f
Aanya <114793986+AanyaPanigrahi@users.noreply.github.com>
2022-12-23 19:31:02 +0530

    Add files via upload

---
+++ 6.loops.py/c.for.py
@@ -0,0 +1,17 @@
+#for loop with range function
+
+#i=0
+#while i < 10:
+    #print('taylor')
+    #i+=1
+for i in range(10): #i=0to9, 10 not included
+    print(f'heyya : {i}')
+
+for i in range(1,11): #i=1to10, starts from 1 & 11 not included
+    print(f'yayy : {i}')
+
+#example: sum from 1 to 10
+total=0
+for i in range(1,11):
+    total+=i
+print(total)
