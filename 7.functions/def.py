35d1ea232d2e46039d5ebdabf4fd89dfc582db3e
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-01-07 00:46:03 +0530

    Update def.py

--- 7.functions/def.py
+++ 7.functions/def.py
@@ -46,3 +46,36 @@ def is_even(numb):
     else:
         return False
 print(is_even(7))
+
+#can also be coded as
+def is_it_even(numo):
+    return numo%2==0 #boolean value
+print(is_it_even(10))
+
+#can also pass no parameter
+def band():
+    return "One Direction"
+print(band())
+
+#EXERCISE- GREATER NUMBER
+def greater(a,b):
+    if a>b:
+        return a
+    return b
+x=int(input('enter first number: '))
+y=int(input('enter second number: '))
+print('greater number is',greater(x,y)) #print(greater(x,y))
+
+def greatest(a,b,c):
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    return c
+print(greatest(4,5,6))
+
+#can also be done using greater func() --> we can call a func inside another
+def new_greatest(p,q,r):
+    bigger=greater(p,q)
+    return greater(bigger,r) #or greater(greater(p,q),r)
+print(new_greatest(10,40,30))
