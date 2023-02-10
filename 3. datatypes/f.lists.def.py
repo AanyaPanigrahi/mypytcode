027479cd705829dc50d61fa32ff5221af015d3e2
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-02-10 07:08:54 +0530

    Add files via upload

---
+++ 3. datatypes/f.lists.def.py
@@ -0,0 +1,46 @@
+nums=list(range(1,6))
+
+def negativelist(l):
+    negative=[]
+    for i in l:
+        negative.append(-i)
+    return negative
+print(negativelist(nums))
+
+def square_list(q):
+    square=[]
+    for i in q:
+        square.append(i**2)
+    return square
+print(list(range(1,11)))
+
+def reverse_list(m):
+    m.reverse()
+    return m
+print(reverse_list(nums))
+
+#def reverselist(l):
+#     return l[::-1]
+
+def negativelist(l):
+    negative=[]
+    for i in l:
+        negative.append(-i)
+    return negative
+print(negativelist(nums))
+
+def square_list(q):
+    square=[]
+    for i in q:
+        square.append(i**2)
+    return square
+print(list(range(1,11)))
+
+def reverse_list(m):
+    m.reverse()
+    return m
+print(reverse_list(nums))
+
+#def reverselist(l):
+#     return l[::-1]
+
