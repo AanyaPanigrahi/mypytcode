f2a5639418f07ef73390599b81575a7ae1c701d0
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-03-27 23:07:10 +0530

    Add files via upload

---
+++ exercise/factorial.py
@@ -0,0 +1,16 @@
+# using for loop
+n=int(input('enter num: '))
+total=1
+i=1
+for i in range(1,n+1):
+    total*=i
+print(total)
+
+# using while loop
+# n=int(input('enter num: '))
+# total=1
+# i=1
+# while i<=n:
+#     total*=i
+#     i+=1
+# print(total)
